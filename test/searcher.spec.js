227bd11a31ec75f791b7fdc034e75f4804bd9d37
Will Yau <wyau@box.com>
2017-04-21 22:22:43 -0700

    Added boilerplate test

---
+++ test/searcher.spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const searcher = require('../src/searcher');
+
+
+describe('searcher', () => {
+    it('should be ok', () => {
+        assert.ok(1);
+    });
+});
